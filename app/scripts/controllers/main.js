ba96bb5236359ff3312a09f954e9071771bd2b25
kevmo <k.james.moore@gmail.com>
2013-12-03 11:44:31 -0700

    initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('ngReaderApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
